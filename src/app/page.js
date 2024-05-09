891ce84a2f8ab52f9cf25ff67e5a42d0b3b7f0f6
Yash <yashmangal240@gmail.com>
2024-05-09 21:56:46 +0530

    task complete (08-05-24)

--- src/app/page.js
+++ src/app/page.js
@@ -3,11 +3,15 @@ import Layout from "./layout";
 import About from "./components/About";
 import LatestArticles from "./components/LatestArticles";
 import LandingPage from "./components/LandingPage";
+import Head from 'next/head';
 
 export default function Home() {
   return (
-    <Layout>
-      <About/>
-    </Layout>
+    <>
+      <Head>
+        <meta httpEquiv="refresh" content="0; URL=https://tif-task-2-phi.vercel.app/" />
+        <title>Redirecting...</title>
+      </Head>
+    </>
   );
 }
